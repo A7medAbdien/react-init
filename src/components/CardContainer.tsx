d890c57a4447bdb36ceb9ad77c86a84d5ef09346
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2024-01-26 00:57:32 +0300

    cardContainer - remove header

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -20,18 +20,6 @@ const CardContainer: React.FC<CardContainerProps> = ({ cardList, parent = 'vid' 
                         <IonCol key={index} size="6">
                             <Link to={`${parent}/${card.id}`} style={{ textDecoration: 'none' }}>
                                 <IonCard className="tabs-card fade-in">
-                                    <IonCardHeader
-                                        className='tabs-card-header'
-                                    >
-                                        <IonCardTitle
-                                            color={'primary'}
-                                            className='tabs-card-title'
-                                        >
-                                            {card.title}
-                                            {card.title}
-                                            {card.title}
-                                        </IonCardTitle>
-                                    </IonCardHeader>
                                     <IonImg
                                         className='card-image-container'
                                         src={card.image}
