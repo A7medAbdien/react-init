fcb5734aaac17f95869525f952a157b5ef49a500
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2024-01-25 22:05:21 +0300

    renaming

--- src/data/data.ts
+++ src/data/data.ts
@@ -11,59 +11,61 @@ import {
 import { Colors, Links, ContactStrings } from './Strings';
 import account from '../../public/imgs/account.svg';
 
-const content = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio numquam maxime sunt sit eaque error cumque sequi expedita accusamus veritatis eveniet, quo magnam, autem possimus deleniti id enim porro molestiae.`
-const image = "https://docs-demo.ionic.io/assets/madison.jpg"
-const video = "https://res.cloudinary.com/dcmthd8bn/video/upload/v1694723512/react-base-vid/Beauty_of_sea_beach_nature_WhatsApp_status_relaxing_video_shorts_bpesvs.mp4"
-const video2 = "https://res.cloudinary.com/dcmthd8bn/video/upload/v1694723012/react-base-vid/Blue_Sky_and_Clouds_Timelapse_0892__Videvo_preview_kx6yal.mp4"
+const _ = {
+    _content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio numquam maxime sunt sit eaque error cumque sequi expedita accusamus veritatis eveniet, quo magnam, autem possimus deleniti id enim porro molestiae.`,
+    _image: "https://docs-demo.ionic.io/assets/madison.jpg",
+    _video: "https://res.cloudinary.com/dcmthd8bn/video/upload/v1694723512/react-base-vid/Beauty_of_sea_beach_nature_WhatsApp_status_relaxing_._video_shorts_bpesvs.mp4",
+    _video2: "https://res.cloudinary.com/dcmthd8bn/video/upload/v1694723012/react-base-vid/Blue_Sky_and_Clouds_Timelapse_0892__Videvo_preview_kx6yal.mp4",
+}
 
 export const CardList: CardType[] = [
     {
         id: '1',
         title: 'Card 1',
         subtitle: 'Subtitle 1',
-        content: content,
-        image: image,
-        video: video,
+        content: _._content,
+        image: _._image,
+        video: _._video,
     },
     {
         id: '2',
         title: 'Card 2',
         subtitle: 'Subtitle 2',
-        content: content,
-        image: image,
-        video: video2,
+        content: _._content,
+        image: _._image,
+        video: _._video2,
     },
     {
         id: '3',
         title: 'Card 3',
         subtitle: 'Subtitle 3',
-        content: content,
-        image: image,
-        video: video,
+        content: _._content,
+        image: _._image,
+        video: _._video,
     },
     {
         id: '4',
         title: 'Card 1',
         subtitle: 'Subtitle 1',
-        content: content,
-        image: image,
-        video: video,
+        content: _._content,
+        image: _._image,
+        video: _._video,
     },
     {
         id: '5',
         title: 'Card 2',
         subtitle: 'Subtitle 2',
-        content: content,
-        image: image,
-        video: video,
+        content: _._content,
+        image: _._image,
+        video: _._video,
     },
     {
         id: '6',
         title: 'Card 3',
         subtitle: 'Subtitle 3',
-        content: content,
-        image: image,
-        video: video,
+        content: _._content,
+        image: _._image,
+        video: _._video,
     },
 ];
 
@@ -74,8 +76,8 @@ export const IconCardList: IconCardType[] = [
         icon: account,
         bgColor: Colors.yellow,
         textColor: Colors.navy,
-        content: content,
-        video: video,
+        content: _._content,
+        video: _._video,
     },
     {
         id: '2',
@@ -83,8 +85,8 @@ export const IconCardList: IconCardType[] = [
         icon: account,
         bgColor: Colors.yellow,
         textColor: Colors.navy,
-        content: content,
-        video: video,
+        content: _._content,
+        video: _._video,
     },
     {
         id: '3',
@@ -92,8 +94,8 @@ export const IconCardList: IconCardType[] = [
         icon: account,
         bgColor: Colors.orange,
         textColor: Colors.yellow,
-        content: content,
-        video: video,
+        content: _._content,
+        video: _._video,
     },
     {
         id: '4',
@@ -101,8 +103,8 @@ export const IconCardList: IconCardType[] = [
         icon: account,
         bgColor: Colors.orange,
         textColor: Colors.yellow,
-        content: content,
-        video: video,
+        content: _._content,
+        video: _._video,
     },
     {
         id: '5',
@@ -110,8 +112,8 @@ export const IconCardList: IconCardType[] = [
         icon: account,
         bgColor: Colors.sky,
         textColor: Colors.orange,
-        content: content,
-        video: video,
+        content: _._content,
+        video: _._video,
     },
     {
         id: '6',
@@ -119,8 +121,8 @@ export const IconCardList: IconCardType[] = [
         icon: account,
         bgColor: Colors.sky,
         textColor: Colors.orange,
-        content: content,
-        video: video,
+        content: _._content,
+        video: _._video,
     },
     {
         id: '7',
@@ -128,8 +130,8 @@ export const IconCardList: IconCardType[] = [
         icon: account,
         bgColor: Colors.navy,
         textColor: Colors.beige,
-        content: content,
-        video: video,
+        content: _._content,
+        video: _._video,
     },
     {
         id: '8',
@@ -137,8 +139,8 @@ export const IconCardList: IconCardType[] = [
         icon: account,
         bgColor: Colors.navy,
         textColor: Colors.beige,
-        content: content,
-        video: video,
+        content: _._content,
+        video: _._video,
     },
 ]
 
