054990f9590346ce1d4b3a20735c126279877fd7
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2024-02-08 03:10:57 +0300

    edit name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'react-init',
+  appId: 'com.aga.menkom',
+  appName: 'Menkom',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
